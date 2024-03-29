310b84d3fd188f27ad852b48b9a69da9cdcfb603
tnphucccc <ktsukasa1412@gmail.com>
2024-03-29 14:11:04 +0700

    feat: changing to tailwindcss and restructured portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
